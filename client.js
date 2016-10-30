6426889248cbe435bd95413caa051acb3ebff3df
laurarundle <rundlel@tcd.ie>
2016-10-30 18:34:04 +0000

    change buffer

--- client.js
+++ client.js
@@ -2,11 +2,12 @@ var net = require("net");
 var socket = new net.Socket();
 
 socket.connect(8000, '10.62.0.140', function(){
+	socket.setEncoding('utf8');
 	console.log("Socket connected");
 	socket.write("GET /server.php?message=helloworld HTTP/1.0\r\n\r\n");
 });
 
 socket.on("data", function(message){
-	message.toString('utf8');
+
 	console.log(message);
 });
