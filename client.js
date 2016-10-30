2b3c83c51ea104aa0768c924cff0cbdd30d001d1
laurarundle <rundlel@tcd.ie>
2016-10-30 18:03:30 +0000

    fix error

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 var net = require("net");
 var socket = new net.Socket();
 
-socket.connect(8000, 10.62.0.140, function(){
+socket.connect(8000, '10.62.0.140', function(){
 	console.log("Socket connected");
 	socket.write("hello world");
 });
