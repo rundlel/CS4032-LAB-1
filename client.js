2d4f98c2c1379eadea93171bf220e1fae71338fb
laurarundle <rundlel@tcd.ie>
2016-10-30 18:19:05 +0000

    edit to socket.write

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ var socket = new net.Socket();
 
 socket.connect(8000, '10.62.0.140', function(){
 	console.log("Socket connected");
-	socket.write("hello world");
+	socket.write("GET /server.php?message=helloworld HTTP1.0\r\n");
 });
 
 socket.on("data", function(message){
